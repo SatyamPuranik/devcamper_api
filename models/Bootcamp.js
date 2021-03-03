c7a14f6f6654c5da14867abaa15c9842b76b1df8
SatyamPuranik <clashersatyam.7@gmail.com>
2021-03-03 15:34:48 +0530

    Cascade Delete
    Populate, Virtuals and Cascade Delete

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -131,11 +131,19 @@ BootcampSchema.pre('save', async function (next) {
   next();
 });
 
+// Cascade delete courses when a bootcamp is deleted
+BootcampSchema.pre('remove', async function (next) {
+  // We can access fields  even when we are deleting is because we are useing many
+  console.log(`Courses being removed from the bootcamp ${this._id}`);
+  await this.model('Course').deleteMany({ bootcamp: this._id });
+  next();
+});
+
 // Reverse populate with virtuals
 BootcampSchema.virtual('courses', {
-  ref: 'Courses',
+  ref: 'Course',
   localField: '_id',
-  foreginField: 'bootcamp',
+  foreignField: 'bootcamp',
   justOne: false
 });
 
