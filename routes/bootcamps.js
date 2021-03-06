1c4277decb76fd903844ffa65f1e411fbf9e25b0
SatyamPuranik <clashersatyam.7@gmail.com>
2021-03-06 23:30:14 +0530

    Advanced Results Middleware

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -10,12 +10,18 @@ const {
   bootcampPhotoUpload
 } = require('../controllers/bootcamps');
 
+const Bootcamp = require('../models/Bootcamp');
+const advancedResults = require('../middleware/advancedResults');
+
 // Include other resource routers
 const courseRouter = require('./courses');
 
 // we now no longer have access to app that was in server.js, we will replace app with router
 // We now dont include /api/v1/bootcamps in this file, because we have linked the route (/api/v1/bootcamps) to this file in server.js. /api/v1/bootcamps is /
-router.route('/').get(getBootcamps).post(createBootcamp); //They take same URL so we can write like this
+router
+  .route('/')
+  .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
+  .post(createBootcamp); //They take same URL so we can write like this
 router
   .route('/:id')
   .get(getBootcamp)
