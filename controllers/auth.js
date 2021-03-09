1df95ebf3da4eadb27e1a62dc32ccbd2897b24b3
SatyamPuranik <clashersatyam.7@gmail.com>
2021-03-09 22:03:41 +0530

    User Login

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ const asyncHandler = require('../middleware/async');
 const User = require('../models/User');
 
 // @desc     Register user
-// @route    GET /api/v1/auth/register
+// @route    POST /api/v1/auth/register
 // @access   Public
 exports.register = asyncHandler(async (req, res, next) => {
   const { name, email, password, role } = req.body;
@@ -18,6 +18,40 @@ exports.register = asyncHandler(async (req, res, next) => {
 
   // Create token
   const token = user.getSignedJwtToken(); //We are using a method not a static. A method is called on a actual user and a static is called on the object itself. {*Same as static function in classes and methods in classes}
+
+  res.status(200).json({
+    success: true,
+    token
+  });
+});
+
+// @desc     Login user
+// @route    POST /api/v1/auth/login
+// @access   Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  //  Validate email and password
+  if (!email || !password) {
+    return next(new ErrorResponse('Please provide an email and password', 400));
+  }
+
+  // Check for user
+  const user = await User.findOne({ email: email }).select('+password');
+
+  if (!user) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  // Check if password matches
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  // Create token
+  const token = user.getSignedJwtToken();
 
   res.status(200).json({
     success: true,
