e8ada97ae0ae0f5f0a5cd9c71bacb91fd1fb31a6
SatyamPuranik <clashersatyam.7@gmail.com>
2021-03-11 11:17:46 +0530

    Role Authorization

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -16,23 +16,25 @@ const advancedResults = require('../middleware/advancedResults');
 // Include other resource routers
 const courseRouter = require('./courses');
 
-const { protect } = require('../middleware/auth');
+const { protect, authorize } = require('../middleware/auth');
 
 // we now no longer have access to app that was in server.js, we will replace app with router
 // We now dont include /api/v1/bootcamps in this file, because we have linked the route (/api/v1/bootcamps) to this file in server.js. /api/v1/bootcamps is /
 router
   .route('/')
   .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
-  .post(protect, createBootcamp); //They take same URL so we can write like this
+  .post(protect, authorize('publisher', 'admin'), createBootcamp); //They take same URL so we can write like this
 router
   .route('/:id')
   .get(getBootcamp)
-  .put(protect, updateBootcamp)
-  .delete(protect, deleteBootcamp);
+  .put(protect, authorize('publisher', 'admin'), updateBootcamp)
+  .delete(protect, authorize('publisher', 'admin'), deleteBootcamp);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
-router.route('/:id/photo').put(protect, bootcampPhotoUpload);
+router
+  .route('/:id/photo')
+  .put(protect, authorize('publisher', 'admin'), bootcampPhotoUpload);
 
 // Re-route into other resource router
 router.use('/:bootcampId/courses', courseRouter);
