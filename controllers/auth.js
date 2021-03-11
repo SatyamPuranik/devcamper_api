1b1708c5acc5c38791fa668f3ec828e92d4e29b9
SatyamPuranik <clashersatyam.7@gmail.com>
2021-03-11 21:35:14 +0530

    Sending email

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const ErrorResponse = require('../utilis/errorResponse');
 const asyncHandler = require('../middleware/async');
 const User = require('../models/User');
+const sendEmail = require('../utilis/sendEmail');
 
 // @desc     Register user
 // @route    POST /api/v1/auth/register
@@ -82,6 +83,34 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
   console.log(resetToken);
 
   await user.save({ validateBeforeSave: false }); //So resetPasswordToken and resetPasswordExpire fields will be stored in DB
+
+  // Create reset url
+  const resetUrl = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/resetpassword/${resetToken}`;
+
+  const message = `You are reciving this email because you (or someone else) has requested the reset of a password. Please make a PUT request to :\n\n ${resetUrl}`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Password reset token',
+      message
+    });
+    res.status(200).json({
+      success: true,
+      data: 'Email sent'
+    });
+  } catch (error) {
+    // If anything goes wrong we want to get rid of resetPasswordToken & resetPasswordExpire fields in database
+    console.log(error);
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save({ validateBeforeSave: false });
+
+    return next(new ErrorResponse('Email could not be sent', 500));
+  }
 
   res.status(200).json({
     success: true,
